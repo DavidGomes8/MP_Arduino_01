7225c05c4b858979303547f72ae33fd81af9721d
David <davidgomes8@hotmail.com>
2019-09-03 12:14:10 -0300

    Arduino na unity

---
+++ Assets/Teste.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    public ArdJoystick.ArdController ardController;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (ardController.GetKeyDown(ArdJoystick.ArdKeyCode.BUTTON_A))
+        {
+        }
+    }
+}
